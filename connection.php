13faec9345f4a853d2f613e25f1c5c71cf513483
Anish8659 <113288927+Anish8659@users.noreply.github.com>
2023-03-03 14:13:20 +0000

    move connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 Database=mysql; Server=bambishoes-server.mysql.database.azure.com; User Id=iyduhmknsq; Password=3LKGE6Y6XM8QJSJV$
 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "bambishoes-server.mysql.database.azure.com", "iyduhmknsq", "3LKGE6Y6XM8QJSJV$", "bambishoes-database", 3306, MYSQLI_CLIENT_SSL);
