67d9316e9e307d06899300ef3fcd6ee5f147f968
Anish8659 <113288927+Anish8659@users.noreply.github.com>
2023-03-25 13:38:37 +0000

    update env and connection.php with latest db details

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
-Database=mysql; Server=bambishoes-server.mysql.database.azure.com; User Id=iyduhmknsq; Password=3LKGE6Y6XM8QJSJV$
+Database=mysql; Server=bambishoes-server.mysql.database.azure.com; User Id=vczksknlfx; Password=8R626IQF8V3U34MS$
 
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "bambishoes-server.mysql.database.azure.com", "iyduhmknsq", "3LKGE6Y6XM8QJSJV$", "bambishoes-database", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "bambishoes-server.mysql.database.azure.com", "vczksknlfx", "8R626IQF8V3U34MS$", "bambishoes-database", 3306, MYSQLI_CLIENT_SSL);
